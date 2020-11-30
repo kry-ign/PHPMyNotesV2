0ca9a5cce4b13acdd9c203047d26eaf36b449d9f
kry.ign93 <kris_ignatiuk@wp.pl>
2020-11-30 20:27:46 +0100

    refactoring index.php create pages list.php and create.php

---
+++ www/templates/pages/list.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <div class="head">
+            <h1>Moje notatki</h1>
+        </div>
+        <div class="container">
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="/">Lista notatek</a>
+                    </li>
+                    <li>
+                        <a href="/?action=create">Nowa notatka</a>
+                    </li>
+                </ul>
+
+            </div>
+            <div>
+                        Lista notatek
+            </div>
+
+        </div>
+        <div class="footer">
+
+        </div>
+    </body>
+</html>
